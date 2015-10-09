f4f1c8eb2d50026b69acb637229a5f1ce619fe12
James <dong.james.sept14@gmail.com>
2015-10-09 14:29:24 -0500

    Add comment markers to token list and add todos

--- Lexer.hs
+++ Lexer.hs
@@ -27,7 +27,12 @@ data Token = T_Identifier String
            | T_Comma
            | T_CommaAt
            | T_Dot
+           | T_BeginComment
+           | T_EndComment
+           | T_CommentOne
            deriving (Show)
+
+-- TODO: implement comments
 
 special_initial = "!$%&*/:<=>?^_~"
 special_subsequent = "+-.@"
