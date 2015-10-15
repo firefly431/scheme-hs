5f5c06f81f05bb6890e3e4f97523bd9adf1e04de
James <dong.james.sept14@gmail.com>
2015-10-15 14:03:31 -0500

    Implement commenting and make parse_one return maybe

--- Parser.hs
+++ Parser.hs
@@ -3,6 +3,7 @@ module Parser
 ) where
 
 import Prelude hiding (lex)
+import Data.Maybe
 
 import Lexer
 import Types
@@ -12,16 +13,25 @@ parse = parse' . lex
 
 parse' :: [Token] -> S_Object
 parse' [] = C_List C_EmptyList
-parse' toks = let (obj, rest) = parse_one toks in C_List $ C_Cons obj (parse' rest)
+parse' toks = case parse_one toks of
+    Just (obj, rest) -> C_List $ C_Cons obj (parse' rest)
+    Nothing -> C_List C_EmptyList
 
-parse_one :: [Token] -> (S_Object, [Token])
+parse_one :: [Token] -> Maybe (S_Object, [Token])
+parse_one [] = Nothing
+parse_one (T_CommentOne : rest) = parse_one rest >>= parse_one . snd
 parse_one (T_LParen : rest) = parse_list_body rest
     where
-        parse_list_body :: [Token] -> (S_Object, [Token])
-        parse_list_body (T_RParen : rest) = (C_List C_EmptyList, rest)
-        parse_list_body (T_Dot : rest) = let (obj, rest') = parse_one rest in if head rest' /= T_RParen then error "expected RParen" else (obj, tail rest')
-        parse_list_body toks = let
-            (obj, rest) = parse_one toks
-            (list, rest') = parse_list_body rest in (C_List $ C_Cons obj list, rest')
+        parse_list_body :: [Token] -> Maybe (S_Object, [Token])
+        parse_list_body (T_RParen : rest) = Just (C_List C_EmptyList, rest)
+        parse_list_body (T_Dot : rest) = do
+            (obj, rest') <- parse_one rest
+            if head rest' /= T_RParen then error "expected RParen" else return (obj, tail rest')
+        parse_list_body toks = do
+            (obj, rest) <- parse_one toks
+            (list, rest') <- parse_list_body rest
+            return (C_List $ C_Cons obj list, rest')
 -- substitute quote syntax for (quote something)
-parse_one (T_Quote : rest) = let (obj, rest) = parse_one rest in (C_List $ C_Cons (C_Symbol "quote") $ C_List $ C_Cons obj $ C_List C_EmptyList, rest)
+parse_one (T_Quote : rest) = do
+    (obj, rest) <- parse_one rest
+    return (C_List $ C_Cons (C_Symbol "quote") $ C_List $ C_Cons obj $ C_List C_EmptyList, rest)
